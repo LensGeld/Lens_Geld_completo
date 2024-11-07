b2f7b918446641cb16e1947e66118f2dd9143a00
Sky Fenix <165224476+LensGeld@users.noreply.github.com>
2024-11-07 00:13:48 +0000

    Adição de controle de Server Flask

--- IA/app.py
+++ IA/app.py
@@ -7,8 +7,12 @@ from flask import Flask, request, jsonify
 # Inicialização do app Flask
 app = Flask(__name__)
 
-cred = credentials.Certificate("/IA/lensgeld-9df15-firebase-adminsdk-d4rin-b36af14a39.json")
-firebase_admin.initialize_app(cred)
+# Caminho para o arquivo de credenciais do Firebase
+cred = credentials.Certificate("/workspaces/Lens_Geld_completo/IA/lensgeld-9df15-firebase-adminsdk-d4rin-b36af14a39.json")
+
+# Verifica se o Firebase já foi inicializado
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
 # Inicialização do Firestore
 db = firestore.client()
@@ -16,11 +20,11 @@ db = firestore.client()
 @app.route('/api/usuario/<user_id>', methods=['GET'])
 def obter_nome_usuario(user_id):
     try:
-        # Busca o documento do usuário no Firestore pela nova coleção 'firstname'
+        # Busca o documento do usuário no Firestore pela coleção 'firstname'
         usuario_ref = db.collection('firstname').document(user_id)
         usuario = usuario_ref.get()
 
-         # Verifica se o usuário existe
+        # Verifica se o usuário existe
         if usuario.exists:
             # Obtém o nome do usuário do documento
             nome = usuario.to_dict().get('nome')
@@ -31,6 +35,7 @@ def obter_nome_usuario(user_id):
     except Exception as e:
         return jsonify({'erro': str(e)}), 500
 
+
 # Chave de API da OpenAI
 openai.api_key = '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'
 
@@ -38,7 +43,7 @@ openai.api_key = 'sk-proj-WhezeJTX_aG_g6ybrBMZRjqqSWnleD-G0D2u0fvZLV5nei1MPDgGeB
 def obter_resposta():
     data = request.get_json()
     pergunta = data.get('pergunta')
-    
+
     # Verifica se a pergunta foi fornecida
     if not pergunta:
         return jsonify({'erro': 'Pergunta não fornecida.'}), 400
@@ -55,5 +60,7 @@ def obter_resposta():
 
     return jsonify({'resposta': resposta})
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Garantir que o Flask aceite conexões de qualquer interface e rodar na porta 5000
+    app.run(debug=True, host='0.0.0.0', port=5000)
